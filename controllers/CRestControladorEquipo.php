7d1e6075992b3471f47a545e8393346ae9fc4f8f
amateobarco <torratedurarco@gmail.com>
2017-06-18 13:39:12 +0200

    Update CRestControladorEquipo.php

--- controllers/CRestControladorEquipo.php
+++ controllers/CRestControladorEquipo.php
@@ -42,7 +42,21 @@ if(isset($_POST['ejecutar'])){
 
 
 
-
+if(isset($_POST['metodo'])) {
+    if ($_POST['metodo'] == "misEquipos") {
+        if($info = $equipo->mostrarEquipoUsuario()){
+            echo json_encode($info);
+            return true;
+        }
+     }
+        
+    if ($_POST['metodo'] == "muestraInfo" && isset($_POST['idEquipo'])) {
+        if($info = $equipo->infoMiEquipo($_POST['idEquipo'])){
+            echo json_encode($info);
+            return true;
+        }
+    }       
+}
 
 
 
